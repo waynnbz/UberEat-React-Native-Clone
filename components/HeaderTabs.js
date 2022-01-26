b645e895f38ab020896bf38a3dbea2c9ee73146b
waynnbz <waylonago@gmail.com>
2022-01-26 23:32:46 +0800

    first commit

---
+++ components/HeaderTabs.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState('Delivery');
+
+  return (
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton text="Delivery" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <HeaderButton text="Pickup" activeTab={activeTab} setActiveTab={setActiveTab} />
+    </View>
+  );
+}
+
+const HeaderButton = (props) => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.text === props.activeTab ? "black" : "white",
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}
+    onPress={() => {props.setActiveTab(props.text)}}
+  >
+    <Text
+      style={{
+        color: props.text === props.activeTab ? "white" : "black",
+        fontSize: 15,
+        fontWeight: "900",
+      }}
+    >
+      {props.text}
+    </Text>
+  </TouchableOpacity>
+);
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+    </SafeAreaView>
+  );
+}
