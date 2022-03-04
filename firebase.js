b913f617ae2ee82167a8bcb39a2374f307cd7964
waynnbz <waylonago@gmail.com>
2022-03-04 23:55:23 +0800

    edited firebase

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDk1tmoChM0cTXG-wg-9RV0ZdOzX6G3wE0",
-  authDomain: "uber-clone-340814.firebaseapp.com",
-  projectId: "uber-clone-340814",
-  storageBucket: "uber-clone-340814.appspot.com",
-  messagingSenderId: "731653946350",
-  appId: "1:731653946350:web:62152ee2151a06760cecff",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDk1tmoChM0cTXG-wg-9RV0ZdOzX6G3wE0",
+//   authDomain: "uber-clone-340814.firebaseapp.com",
+//   projectId: "uber-clone-340814",
+//   storageBucket: "uber-clone-340814.appspot.com",
+//   messagingSenderId: "731653946350",
+//   appId: "1:731653946350:web:62152ee2151a06760cecff",
+// };
 
-// `!` converts the value into boolean
-!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+// // `!` converts the value into boolean
+// !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
-export default firebase;
+// export default firebase;
